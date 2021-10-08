85a0e01a6af960f2d54158a1bf84dcf93a23982f
Francisco-Isganzella <FRANCISCO8ISG@GMAIL.COM>
2021-10-08 13:38:17 -0300

    Alterações no construtor
    Mudei o JFrame para o construtor apenas.

--- src/sysrate/SysRate.java
+++ src/sysrate/SysRate.java
@@ -13,35 +13,21 @@ import javax.swing.JFrame;
  * @author franc
  */
 public class SysRate {
-<<<<<<< Updated upstream
-
-    public SysRate(){
-        janela();
-    }
-    
-    public JFrame janela(){
-        JFrame janela = new JFrame("SysRate");
-=======
     
     private JFrame janela;
     
     public SysRate(){
-        janela = new JFrame("SysRate");
->>>>>>> Stashed changes
-        janela.setSize(500,650);
-        janela.getContentPane().setBackground(Color.orange);
-        janela.setVisible(true);
-        janela.setLocationRelativeTo(null);
-        janela.setLayout(null);
-        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< Updated upstream
-        
-        return janela;
-=======
-        janela.setResizable(false);
-        
->>>>>>> Stashed changes
+    janela = new JFrame("SysRate");
+    janela.setSize(500,650);
+    janela.getContentPane().setBackground(Color.orange);
+    janela.setVisible(true);
+    janela.setLocationRelativeTo(null);
+    janela.setLayout(null);
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    janela.setResizable(false);
+
     }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         new SysRate();
