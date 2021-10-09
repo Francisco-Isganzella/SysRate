17af9b055120a8259447aa990c6affa559ffe20f
Francisco-Isganzella <FRANCISCO8ISG@GMAIL.COM>
2021-10-09 14:51:30 -0300

    Janela reutilizavel
    Alterei o tamanho da janela base;
    Deixei ela em um metodo onde podemos reutilizar em qualquer classe que quisermos;
    Deixei uma classe criada como exemplo de como reutilizar a janela em outra classe.

---
+++ src/sysrate/Cadastro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sysrate;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author franc
+ */
+public class Cadastro {
+    
+    private JFrame janela;
+    
+    public Cadastro(){
+        
+        janela = new SysRate().janela();
+        
+    }
+    
+}
--- src/sysrate/SysRate.java
+++ src/sysrate/SysRate.java
@@ -17,21 +17,27 @@ public class SysRate {
     private JFrame janela;
     
     public SysRate(){
-    janela = new JFrame("SysRate");
-    janela.setSize(500,650);
-    janela.getContentPane().setBackground(Color.orange);
-    janela.setVisible(true);
-    janela.setLocationRelativeTo(null);
-    janela.setLayout(null);
-    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    janela.setResizable(false);
-
+        
+        janela();
+        
+    }
+    
+    public JFrame janela(){
+        janela = new JFrame("SysRate");
+        janela.setSize(1000,650);
+        janela.getContentPane().setBackground(Color.orange);
+        janela.setVisible(true);
+        janela.setLocationRelativeTo(null);
+        janela.setLayout(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setResizable(false);
+        
+        return janela;
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
         new SysRate();
-        
     }
     
 }
